4e77b50e68fc1ba0f85b792dba21bf498d91a352
Ramon Sanchez <songwright66@gmail.com>
2019-05-31 13:59:48 -0700

    feat: Add saving data function to form

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 // create element and render cafe
 function renderCafe(doc) {
@@ -22,3 +23,14 @@ db.collection('cafes').get().then((snapshot) => {
       renderCafe(doc);
     })
 });
+
+// Save data.
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // Prevent submission from refreshing the page.
+  db.collection('cafes').add({ // Add name and city to cafes collection.
+    name: form.name.value,
+    city: form.city.value
+  });
+  form.name.value = ''; // Clear values from the input fields.
+  form.city.value = '';
+})
