638eb73e2ab0c2a1b0e47fc65c3c2bcdeaf58430
Ramon Sanchez <songwright66@gmail.com>
2019-05-31 12:01:59 -0700

    feat: Get doc data into console.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Get the cafes collection and docs.
 db.collection('cafes').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
-      console.log(doc);
+      console.log(doc.data());
     })
 });
